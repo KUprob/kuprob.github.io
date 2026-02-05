0af0dfee5e7653d8daff66ace9ff2d6702734f6c
tejaswi005 <123118675+tejaswi005@users.noreply.github.com>
2026-02-05 15:41:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const events = [
-    { title: "Zhipeng Liu", date: "2026-01-30", abstract: "KPZ fixed point with a general initial condition of compact support", room: "Zoom meeting" },
-    { title: "Zhipeng Liu", date: "2026-02-06", abstract: "KPZ fixed point with a general initial condition of compact support, part 2", room: "Zoom meeting" },
+    { title: "Zhipeng Liu", date: "2026-01-30", abstract: "KPZ fixed point with a general initial condition of compact support (Part 1)", room: "Zoom meeting" },
+    { title: "Zhipeng Liu", date: "2026-02-06", abstract: "KPZ fixed point with a general initial condition of compact support (Part 2)", room: "Zoom meeting" },
     { title: "TBA", date: "2026-02-13", abstract: "NA", room: "Zoom meeting" },
     { title: "TBA", date: "2026-02-20", abstract: "NA", room: "Zoom meeting" },
     { title: "TBA", date: "2026-02-27", abstract: "NA", room: "Zoom meeting" },
